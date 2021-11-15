dfb8b9be165a0a14beadd7fdc3596e27cf6cd44a
Cihan İçelliler <icellilercihan@gmail.com>
2021-11-15 21:23:25 +0300

    v1.3.1 -comment lines removed

--- sudokuGame.py
+++ sudokuGame.py
@@ -1,29 +1,24 @@
-# import pygame library
 from threading import Thread
 import pygame
 from samurai import *
 from multiprocessing import Process
-# initialise the pygame font
+
 pygame.font.init()
 
-# Total window
 screen = pygame.display.set_mode((770, 770))
 
-# Title and Icon
 pygame.display.set_caption("SAMURAI SUDOKU SOLVER")
 
 x = 0
 y = 0
 dif = 330 / 9
 val = 0
-# Default Sudoku Board
 
 f = open(r"C:\Users\icell\Desktop\Programlama\Python\SamuraiSudokuSolver\tests\easy1.txt", 'r')
 grid = changeTextFileFormat(f)
 
-# Load test fonts for future use
 font1 = pygame.font.SysFont("comicsans", 10)
-font2 = pygame.font.SysFont("comicsans", 10)
+font2 = pygame.font.SysFont("comicsans", 10, True)
 
 
 def get_cord(pos):
@@ -31,8 +26,6 @@ def get_cord(pos):
     x = pos[0]//dif
     global y
     y = pos[1]//dif
-
-# Highlight the cell selected
 
 
 def draw_box():
@@ -42,12 +35,8 @@ def draw_box():
         pygame.draw.line(screen, (255, 0, 0), ((x + i) * dif,
                                                y * dif), ((x + i) * dif, y * dif + dif), 7)
 
-# Function to draw required lines for making Sudoku grid
-
-
 def draw():
-
-    # Draw the lines
+    
     for i in range(21):
         for j in range(21):
 
@@ -55,14 +44,11 @@ def draw():
                 pygame.draw.rect(screen, (20, 20, 31),
                                  (j * dif, i * dif, dif + 1, dif + 1))
             if grid[i][j] != "0" and grid[i][j] != ".":
-                # Fill blue color in already numbered grid
                 pygame.draw.rect(screen, (0, 153, 153),
                                  (j * dif, i * dif, dif + 1, dif + 1))
-                # Fill grid with default numbers specified
                 text1 = font1.render(str(grid[i][j]), 1, (0, 0, 0))
                 screen.blit(text1, (j * dif + 15, i * dif + 15))
 
-    # Draw lines horizontally and verticallyto form grid
     for i in range(25):
         if i % 3 == 0:
             thick = 7
@@ -79,7 +65,7 @@ def draw_a(values, sqr):
     for i in range(9):
         for j in range(9):
             time.sleep(1)
-            text = font1.render(values[sqr[count]], 10, (0, 255, 0))
+            text = font2.render(values[sqr[count]], 10, (0, 255, 0))
             screen.blit(text, (j * dif + 15, i * dif + 15))
             count += 1
             print("a")
@@ -91,7 +77,7 @@ def draw_b(values, sqr):
     for i in range(9):
         for j in range(12, 21):
             time.sleep(1)
-            text = font1.render(values[sqr[count]], 10, (0, 255, 0))
+            text = font2.render(values[sqr[count]], 10, (0, 255, 0))
             screen.blit(text, (j * dif + 15, i * dif + 15))
             count += 1
             print("b")
@@ -103,7 +89,7 @@ def draw_c(values, sqr):
     for i in range(12, 21):
         for j in range(9):
             time.sleep(1)
-            text = font1.render(values[sqr[count]], 10, (0, 255, 0))
+            text = font2.render(values[sqr[count]], 10, (0, 255, 0))
             screen.blit(text, (j * dif + 15, i * dif + 15))
             count += 1
             print("c")
@@ -115,7 +101,7 @@ def draw_d(values, sqr):
     for i in range(12, 21):
         for j in range(12, 21):
             time.sleep(1)
-            text = font1.render(values[sqr[count]], 10, (0, 255, 0))
+            text = font2.render(values[sqr[count]], 10, (0, 255, 0))
             screen.blit(text, (j * dif + 15, i * dif + 15))
             count += 1
             print("d")
@@ -127,7 +113,7 @@ def draw_plus(values, sqr):
     for i in range(6, 15):
         for j in range(6, 15):
             time.sleep(1)
-            text = font1.render(values[sqr[count]], 10, (0, 255, 0))
+            text = font2.render(values[sqr[count]], 10, (0, 255, 0))
             screen.blit(text, (j * dif + 15, i * dif + 15))
             count += 1
             print("plus")
@@ -139,11 +125,8 @@ ans = solve(grid)
 display_samurai(ans)
 
 while run:
-    # White color background
     screen.fill((255, 255, 255))
-    # Loop through the events stored in event.get()
     for event in pygame.event.get():
-        # Quit the game window
         if event.type == pygame.QUIT:
             run = False
             if event.key == pygame.K_RETURN:
@@ -160,7 +143,5 @@ while run:
     t4.join()
     t5.join()
     pygame.display.update() 
-# Update window
 
-# Quit pygame window
 pygame.quit()
